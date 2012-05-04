f5bcceb0da8d2646dc99c2a4e17d3ed74358056e
Jens Nistler <mail@jensnistler.de>
2012-05-04 15:40:07 +0200

    Fixed brightness handling

--- src/de/jensnistler/routemap/activities/Map.java
+++ src/de/jensnistler/routemap/activities/Map.java
@@ -46,9 +46,8 @@ public class Map extends MapActivity implements LocationListener {
     private boolean mPreferenceStandby;
     private boolean mPreferenceRotateMap;
     private String mPreferenceBrightness;
-    private int mUserBrightnessMode;
-    private float mUserBrightnessValue;
     private String mPreferenceRouteFile;
+    private int mUserBrightnessMode;
 
     private MapView mMapView;
     private RotateViewGroup mMapViewGroup; 
@@ -90,59 +89,25 @@ public class Map extends MapActivity implements LocationListener {
         setContentView(mViewGroup);
     }
 
-    /**
-     * load preferences on start
-     */
-    protected void onStart() {
-        // save user brightness
-        try {
-            mUserBrightnessMode = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE);
-            if (Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL == mUserBrightnessMode) {
-                WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
-                mUserBrightnessValue = layoutParams.screenBrightness;
-            }
-        }
-        catch (Settings.SettingNotFoundException e) {
-            // setting not found, default can not be restored
-            mUserBrightnessMode = -1;
-            mUserBrightnessValue = -1.0f;
-        }
-
-        handlePreferences();
-        super.onStart();
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
-        handlePreferences();
-        initializeLocationAndStartGpsThread();
-    }
 
-    @Override
-    protected void onStop() {
-        mThread.interrupt();
-
-        // reset standby mode
-        if (true == mPreferenceStandby) {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        }
-
-        // reset brightness to user default
-        restoreBrightness();
-
-        super.onStop();
-    }
-
-    /**
-     * handle preferences and change system values
-     */
-    private void handlePreferences() {
+        // load preference data
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         mPreferenceStandby = prefs.getBoolean("standby", false);
         mPreferenceRotateMap = prefs.getBoolean("rotateMap", false);
         mPreferenceBrightness = prefs.getString("brightness", BRIGHTNESS_NOCHANGE).trim();
         mPreferenceRouteFile = prefs.getString("routeFile", null);
+
+        // save user brightness
+        try {
+            mUserBrightnessMode = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE);
+        }
+        catch (Settings.SettingNotFoundException e) {
+            // setting not found, default can not be restored
+            mUserBrightnessMode = -1;
+        }
 
         // disable standby
         if (true == mPreferenceStandby) {
@@ -154,11 +119,39 @@ public class Map extends MapActivity implements LocationListener {
             mMapViewGroup.setHeading(0.0f);
         }
 
+        loadRouteFile();
+        handleBrightnessPreferences();
+
+        // start gps thread
+        initializeLocationAndStartGpsThread();
+    }
+
+    /**
+     * restore settings on pause
+     */
+    @Override
+    protected void onPause() {
+        // reset standby mode
+        if (true == mPreferenceStandby) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+
+        // reset brightness to user default
+        restoreBrightness();
+
+        // stop gps update handler
+        mThread.interrupt();
+
+        super.onPause();
+    }
+
+    private void loadRouteFile() {
         // load route file
         if (null != mPreferenceRouteFile) {
             File routeFile = new File(mPreferenceRouteFile.trim());
             if (!routeFile.exists()) {
                 Toast.makeText(this, "Failed to load route file: " + mPreferenceRouteFile, Toast.LENGTH_LONG).show();
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
                 SharedPreferences.Editor editor = prefs.edit();
                 editor.putString("routeFile", null);
                 editor.commit();
@@ -167,21 +160,23 @@ public class Map extends MapActivity implements LocationListener {
                 renderRouteFile(routeFile);
             }
         }
+    }
 
+    /**
+     * handle preferences and change system values
+     */
+    private void handleBrightnessPreferences() {
         // restore user settings
         if (mPreferenceBrightness.equals(BRIGHTNESS_NOCHANGE)) {
             restoreBrightness();
-            return;
         }
-
         // automatic mode
-        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
-        if (mPreferenceBrightness.equals(BRIGHTNESS_AUTOMATIC)) {
+        else if (mPreferenceBrightness.equals(BRIGHTNESS_AUTOMATIC)) {
             Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
-            layoutParams.screenBrightness = -1.0f;
         }
         // manual mode
         else {
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
             Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
             if (mPreferenceBrightness.equals(BRIGHTNESS_MAXIMUM)) {
                 layoutParams.screenBrightness = 1.0f;
@@ -192,24 +187,23 @@ public class Map extends MapActivity implements LocationListener {
             else if (mPreferenceBrightness.equals(BRIGHTNESS_LOW)) {
                 layoutParams.screenBrightness = 0.2f;
             }
-            else {
-                layoutParams.screenBrightness = -1.0f;
-            }
+            getWindow().setAttributes(layoutParams);
         }
-        getWindow().setAttributes(layoutParams);
     }
 
     private void restoreBrightness() {
-        if (-1 == mUserBrightnessMode) {
+        if (Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL != mUserBrightnessMode && Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC != mUserBrightnessMode) {
             Toast toast = Toast.makeText(this, "Failed to restore brightness settings", Toast.LENGTH_LONG);
             toast.show();
             return;
         }
 
         Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, mUserBrightnessMode);
-        WindowManager.LayoutParams layout = getWindow().getAttributes();
-        layout.screenBrightness = mUserBrightnessValue;
-        getWindow().setAttributes(layout);
+        if (mUserBrightnessMode == Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL) {
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+            layoutParams.screenBrightness = -1.0f;
+            getWindow().setAttributes(layoutParams);
+        }
     }
 
     /**
